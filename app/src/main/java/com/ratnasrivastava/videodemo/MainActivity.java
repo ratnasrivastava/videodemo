2df3401105ec57052a096dbb2319261b76ba4f35
Ratna Srivastava <er.ratnasrivastava@gmail.com>
2020-06-29 22:28:47 +0530

    ADDED :: Initial commit

---
+++ app/src/main/java/com/ratnasrivastava/videodemo/MainActivity.java
@@ -0,0 +1,22 @@
+package com.ratnasrivastava.videodemo;
+
+import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        VideoView videoView = (VideoView) findViewById(R.id.videoView);
+        videoView.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.demo);
+        MediaController mediaController = new MediaController(this);
+        mediaController.setAnchorView(videoView);
+        videoView.setMediaController(mediaController);
+        videoView.start();
+    }
+}
